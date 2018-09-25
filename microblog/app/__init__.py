9aa898f34dcfef6bbe580bd1b2ddbcf9cc341b9f
kanishkd4 <kanishkd4@gmail.com>
2018-09-25 17:14:43 +0530

    created a config file and key

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__) # __name__ is a predifined python variable and is set to the name of the module it is used
+app.config.from_object(Config)
 
 from app import routes
 
