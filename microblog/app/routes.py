960fa9d632dc13a73e07d1d82d291a421dda961d
kanishkd4 <kanishkd4@gmail.com>
2018-09-25 15:17:20 +0530

    first template

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -3,19 +3,12 @@
 # view functions are mapped to one or more route URL's so that flask knows what logic to execute when a client requests a specific URL
 
 from app import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
     user = {"username": "miguel"}
-    return """
-<html>
-    <head>
-        <title>Home page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, """ + user["username"] + """!</h1>
-    </body>
-</html>"""
+    return render_template("index.html", title="Home", user=user)
 
 # we want to expand the view function to show a complete HTML page.
