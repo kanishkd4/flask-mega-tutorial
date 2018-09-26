20c717034d69973a52f43670978717573f8b8be6
kanishkd4 <kanishkd4@gmail.com>
2018-09-26 10:01:12 +0530

    created user loader function

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # __name__ is a predifined python variable and is set to the name of the module it is used
 app = Flask(__name__) 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
