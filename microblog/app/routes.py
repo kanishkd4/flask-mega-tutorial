37d48420b0db829cd31a26dd06e749cf81f765aa
kanishkd4 <kanishkd4@gmail.com>
2018-09-26 11:58:23 +0530

    added () to DataRequired() and successfully written till user logins

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -7,7 +7,7 @@ from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 from app.models import User
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from flask import request
 from werkzeug.urls import url_parse
 
@@ -15,7 +15,6 @@ from werkzeug.urls import url_parse
 @app.route("/index")
 @login_required
 def index():
-    user = {"username": "miguel"}
     posts = [{
         "author": {"username": "John"},
         "body": "Beautiful day in portland"
@@ -24,7 +23,7 @@ def index():
             "author": {"username": "Susan"},
             "body": "The avengers was so cool"
         }]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home", posts=posts)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -40,7 +39,7 @@ def login():
         next_page = request.args.get("next")
         if not next_page or url_parse(next_page).netloc != "":
             next_page = url_for("index")
-        return redirect(url_for(next_page))
+        return redirect(next_page)
     return render_template("login.html", title="Sign in", form=form)
 
 @app.route("/logout")
