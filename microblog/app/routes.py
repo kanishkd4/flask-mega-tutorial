8182980e27766146ce035f393e0d0e100800730c
kanishkd4 <kanishkd4@gmail.com>
2018-09-26 12:27:11 +0530

    chapter 5 over; successful user registration

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -3,9 +3,9 @@
 # view functions are mapped to one or more route URL's so that flask knows what logic to execute 
 # when a client requests a specific URL
 
-from app import app
+from app import app, db
 from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 from flask import request
@@ -46,3 +46,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now a registered user")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
