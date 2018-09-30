dfcdfa5a8f1d60ba3131f34710d6a7a6a26e923b
kanishkd4 <kanishkd4@gmail.com>
2018-09-30 16:13:43 +0530

    changed fake posts to db query

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -29,14 +29,7 @@ def index():
         db.session.commit()
         flash("Your post is now live!")
         return redirect(url_for("index"))
-    posts = [{
-        "author": {"username": "John"},
-        "body": "Beautiful day in portland"
-        },
-        {
-            "author": {"username": "Susan"},
-            "body": "The avengers was so cool"
-        }]
+    posts = current_user.followed_posts().all()
     return render_template("index.html", title="Home Page", form=form, posts=posts)
 
 @app.route("/login", methods=["GET", "POST"])
